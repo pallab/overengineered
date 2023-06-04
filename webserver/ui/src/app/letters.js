bbad45bcb627a228121a4cd467b6257011503f50
Pallab Kalita <13886153+pallab@users.noreply.github.com>
2023-06-04 17:34:24 +0530

    letters for ui

--- webserver/ui/src/app/letters.js
+++ webserver/ui/src/app/letters.js
@@ -26,27 +26,30 @@ const h = () => {
 }
 const e = () => {
     let a = num_range(2, 8).flatMap((y) => {
-        return [XY(2, y), XY(6, y)]
+        return [XY(2, y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
+    let b = num_range(3, 5).flatMap((x) => {
+        return [XY(x,2), XY(x,5), XY(x,8)]
+    });;
     return a.concat(b)
 }
 const l = () => {
     let a = num_range(2, 8).flatMap((y) => {
-        return [XY(2, y), XY(6, y)]
+        return [XY(2, y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
+    let b = [XY(3, 8), XY(4, 8), XY(5, 8)];
     return a.concat(b)
 }
-
 const o = () => {
-    let a = num_range(2, 8).flatMap((y) => {
+    let a = num_range(3, 7).flatMap((y) => {
         return [XY(2, y), XY(6, y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
+    let b = num_range(3, 5).flatMap((x) => {
+        return [XY(x,2), XY(x,8)]
+    });
     return a.concat(b)
 }
 const w = () => {
@@ -54,31 +57,37 @@ const w = () => {
         return [XY(2, y), XY(6, y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
+    let b = [XY(3, 7), XY(4, 6), XY(5, 7)];
     return a.concat(b)
 }
 const r = () => {
     let a = num_range(2, 8).flatMap((y) => {
-        return [XY(2, y), XY(6, y)]
+        return [XY(2, y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
-    return a.concat(b)
+    let b = num_range(3, 5).flatMap((x) => {
+        return [XY(x,2), XY(x,5)]
+    });
+
+    let c = [XY(5, 3), XY(5, 4), XY(3, 6), XY(4, 7), XY(5, 8)];
+    return a.concat(b).concat(c)
 }
 const d = () => {
-    let a = num_range(2, 8).flatMap((y) => {
-        return [XY(2, y), XY(6, y)]
+    let a = num_range(3, 7).flatMap((y) => {
+        return [XY(2, y), XY(6,y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
+    let b = num_range(2, 5).flatMap((x) => {
+        return [XY(x,2), XY(x,8)]
+    });
     return a.concat(b)
 }
 const exclm = () => {
-    let a = num_range(2, 8).flatMap((y) => {
-        return [XY(2, y), XY(6, y)]
+    let a = num_range(2, 6).flatMap((y) => {
+        return [XY(4, y)]
     });
 
-    let b = [XY(3, 5), XY(4, 5), XY(5, 5)];
+    let b = [XY(4, 8)];
     return a.concat(b)
 }
 
